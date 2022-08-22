68699c6f3dbcc5cdd2b336959ebaf5fee9170934
DanielFloria <daniflorialopez@gmail.com>
2022-08-23 01:05:33 +0200

    Modificaciones

--- src/main/java/com/crud/controller/EmpleadoController.java
+++ src/main/java/com/crud/controller/EmpleadoController.java
@@ -9,11 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.dto.Empleado;
 import com.crud.service.EmpleadoServiceImpl;
 
-
+@RestController
+@RequestMapping("/api")
 public class EmpleadoController {
 	
 	@Autowired
